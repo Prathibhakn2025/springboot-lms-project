23a178dc917e7e6af6a5e98b467ff7dc39498410
Jyothin2026 <jyothi.n@atsglobal.in>
2026-01-30 17:59:50 +0530

    structure

---
+++ lms/src/main/java/com/ats/lms/backend/trainerEnrollMgmt/service/TrainerEnrollmentService.java
@@ -0,0 +1,10 @@
+package com.ats.lms.backend.trainerEnrollMgmt.service;
+
+public class TrainerEnrollmentService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
